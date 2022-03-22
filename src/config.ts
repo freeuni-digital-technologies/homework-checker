671c60da897522c6ec47bbc1f5458bf527306d83
Luka <57495732+lukaaduashvili@users.noreply.github.com>
2022-03-22 13:41:53 +0400

    14-IMPROVEMENT Move subject name from config.ts to data/subject.json (#16)
    * 14-IMPROVEMENT Move subject name from config.ts to data/subject.json
    
    * HOTFIX circular import
    
    Co-authored-by: ia rigby <marvinzem@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import path from 'path'
+import fs from "fs";
 
 // ამ ფაილში უნდა იყოს მხოლოდ და მხოლოდ default პარამეტრები 
 // იმ ყველაფრის, რაც user-ს შეუძლია რომ გადმოაწოდოს
@@ -9,7 +10,7 @@ export const results_path = `${data_path}/output`
 export const submissions_path = `${data_path}/submissions`
 
 export const config = {
-    subject: '21f შესავალი ციფრულ ტექნოლოგიებში',
+    subject: JSON.parse(fs.readFileSync(path.resolve(__dirname, `${data_path}/subject.json`)).toString()).subject,
     STUDENTS_DATA_PATH: path.resolve(__dirname, `${data_path}/students.json`),
     CLASSROOM_CREDENTIALS_PATH: path.resolve(__dirname, `${data_path}/credentials/credentials.json`),
     CLASSROOM_TOKEN_PATH: path.resolve(__dirname, `${data_path}/credentials/token.json`)
